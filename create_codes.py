c9318b050fc8c40358362ca4921686abf65270ef
harpone <heikki.a.arponen@gmail.com>
2020-11-13 10:54:40 +0200

    4 workers

--- create_codes.py
+++ create_codes.py
@@ -131,7 +131,7 @@ if __name__ == '__main__':
 
     limit_to = 1000
     num_gpus = 1
-    num_workers = 2
+    num_workers = 4
     upload_to_storage = True
 
     urls_path = 'https://storage.googleapis.com/cvdf-datasets/oid/open-images-dataset-train0.tsv'  # about 1 min
