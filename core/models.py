4abd746de76264ac53170f4b51ee2b0017fe9117
harpone <heikki.a.arponen@gmail.com>
2020-11-22 21:52:40 +0200

    clean up deprecated code

--- core/models.py
+++ core/models.py
@@ -153,32 +153,3 @@ class SuperModel(nn.Module):
         return result_dict
 
 
-
-def load_models():
-    #### load segnet (detectron2), repnet, PCA model (already trained with imagenet data):
-    cfg_fname = 'COCO-PanopticSegmentation/panoptic_fpn_R_101_3x.yaml'
-    cfg = get_cfg()
-    cfg.MODEL.DEVICE = 'cpu'  # force CPU
-    cfg.merge_from_file(model_zoo.get_config_file(cfg_fname))
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-    cfg.INPUT.MIN_SIZE_TEST = 256  # default is 800 and is pretty slow... NOTE this should be same as in augs!!
-    #cfg.INPUT.MAX_SIZE_TEST = 640
-    # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
-    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(cfg_fname)
-
-    # segnet:
-    segnet = DefaultPredictor(cfg)
-
-    with torch.no_grad():
-        # repnet:
-        repnet = resnet50x4()  # TODO: no longer CUDA because I want CPU
-        repnet_pth = './model_data/resnet50-4x.pth'  # TODO: from GCS or cache
-        state_dict = torch.load(repnet_pth)['state_dict']
-        repnet.load_state_dict(state_dict)
-        repnet.eval()
-        repnet.cuda()
-
-    # pca model (for per item codes):
-    pca = load('./model_data/pca_simclr_8192.joblib')  # TODO: from GCS or cache
-
-    return dict(segnet=segnet, repnet=repnet, pca=pca)
