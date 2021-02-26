b5d4d7f062ba535d51c12b7537550e14c92b69dc
harpone <heikki.a.arponen@gmail.com>
2021-02-27 00:22:54 +0200

    asdasdfasdfasfd

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ app.config.update(
     UPLOADED_PATH='./static/cache',
     # Flask-Dropzone config:
     DROPZONE_ALLOWED_FILE_TYPE='image',
-    DROPZONE_MAX_FILE_SIZE=3,
+    DROPZONE_MAX_FILE_SIZE=10,
     DROPZONE_MAX_FILES=1,
     DROPZONE_REDIRECT_VIEW='query_image',  # set redirect view
     #DROPZONE_REDIRECT_VIEW=None,
@@ -152,9 +152,12 @@ def index():
     reset_session()
     if request.method == 'POST':
         f = request.files.get('file')  # hmm OK redirects right after this so no time to process the stuff below??
+        print('FILE OBTAINED')
         #session['f'] = deepcopy(f)
         session['filename'] = f.filename
-        f.save(os.path.join(app.config['UPLOADED_PATH'], session['filename']))
+        print('SESSION FILENAME SAVED')
+        f.save(os.path.join(app.config['UPLOADED_PATH'], session['filename']))  # FFUU this line is NEVER run!
+        print('FILE SAVED')
         #uploaded_image = Image.open(f).convert('RGB')
         # if uploaded_image is None:
         #     flash('Upload size exceeded! Please only use smaller than 10MB size images.')
